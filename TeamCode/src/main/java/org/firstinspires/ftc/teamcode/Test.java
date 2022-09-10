0b007f9e0029b78a357a6c8424217a9cf9b6b597
madelyndm <madelyn.mcgurk@circuitrunners.com>
2022-09-10 14:54:32 -0400

    learning programming

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class Test extends LinearOpMode {
+
+    private DcMotor leftFront;
+
+    @Override
+    public void runOpMode(){
+        leftFront = hardwareMap.get(DcMotor.class, "lf");
+
+        waitForStart();
+        leftFront.setPower(0.5);
+    }
+}
