96678d6d9de7ea3880a282c2a613bb8e8645bf8b
madelyndm <madelyn.mcgurk@circuitrunners.com>
2022-10-11 16:58:47 -0400

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -35,7 +35,7 @@ public class DriveConstants {
      */
     public static final boolean RUN_USING_ENCODER = true;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+            12.7);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -87,10 +87,10 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 43.73576590275441;
-    public static double MAX_ACCEL = 43.73576590275441;
-    public static double MAX_ANG_VEL = Math.toRadians(185.6203555555556);
-    public static double MAX_ANG_ACCEL = Math.toRadians(185.6203555555556);
+    public static double MAX_VEL = 30;
+    public static double MAX_ACCEL = 30;
+    public static double MAX_ANG_VEL = Math.toRadians(180);
+    public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
 
     public static double encoderTicksToInches(double ticks) {
