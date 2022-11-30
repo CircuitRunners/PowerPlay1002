6554d46130953cb0d7af12993186fe6b697234be
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2022-11-29 23:19:51 -0500

    feat: meep meep but me

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingRyan.java
@@ -0,0 +1,55 @@
+package com.example.meepmeeptesting;
+
+import static java.lang.Math.toRadians;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTestingRyan {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        System.setProperty("sun.java2d.opengl", "true");
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setDimensions(14, 14)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(40, 40, toRadians(220), toRadians(220), 11.78)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(37, -63, toRadians(90)))
+                                .splineTo(new Vector2d(36, -30), toRadians(90))
+                                .splineTo(new Vector2d(36, -18), toRadians(94))
+                                .splineToSplineHeading(new Pose2d(29.5, -17, toRadians(-133)), toRadians(-135))
+                                .waitSeconds(2) //drop preload
+
+                                .setReversed(true)
+                                .splineTo(new Vector2d(35, -13), toRadians(43.1)) //back from pole
+                                .splineToSplineHeading(new Pose2d(43, -11.7, toRadians(0)), toRadians(0))
+                                .splineTo(new Vector2d(60, -11.7), toRadians(0)) //to stack
+                                .waitSeconds(1) //at stack
+
+                                
+                                .splineTo(new Vector2d(40, -11.7), toRadians(-180))
+                                .splineToSplineHeading(new Pose2d(29.5, -16.5, toRadians(-130)), toRadians(-135))
+                                .waitSeconds(1) //at pole 1
+
+                                .splineToSplineHeading(new Pose2d(40, -11.7, toRadians(0)), toRadians(0)) //to stack
+                                .splineTo(new Vector2d(60, -11.7), toRadians(0))
+                                .waitSeconds(1) //at stack 1
+
+                                //repeat back to pole
+
+                                .build()
+
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
