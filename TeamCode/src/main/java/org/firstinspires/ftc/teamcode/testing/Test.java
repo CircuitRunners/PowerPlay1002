84f832fc34de15522152916a6ac9b24d310a5ab7
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2022-12-31 23:10:13 -0500

    disabling more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Test.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-
+@Disabled
 @Autonomous
 public class Test extends LinearOpMode {
 
