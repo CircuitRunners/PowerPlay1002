364da3531f46702d12659a88e298fb4d13ad8dc2
madelyndm <madelyn.mcgurk@circuitrunners.com>
2023-07-10 11:44:06 -0400

    CRI Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/cycle/high/CRIMiddleLeftHighAuto.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.auto.cycle.high;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.commands.BulkCacheCommand;
+import org.firstinspires.ftc.teamcode.commands.autocommands.CRIMiddleLeftParkCommand;
+import org.firstinspires.ftc.teamcode.commands.autocommands.DropPoleCommand;
+import org.firstinspires.ftc.teamcode.commands.autocommands.DropPreloadCommand;
+import org.firstinspires.ftc.teamcode.commands.autocommands.GoToStackCommand;
+import org.firstinspires.ftc.teamcode.commands.autocommands.ParkCommand;
+import org.firstinspires.ftc.teamcode.commands.autocommands.ThreeCycleTrajectories;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
+import org.firstinspires.ftc.teamcode.vision.BeaconDetector;
+
+
+@Autonomous(name = "MiddleLeft H", group = "High")
+public class CRIMiddleLeftHighAuto extends CommandOpMode {
+
+
+    private SampleMecanumDrive drive;
+    private Claw claw;
+    private Lift lift;
+    private Arm arm;
+
+
+    private BeaconDetector beaconDetector;
+    private BeaconDetector.BeaconTags beaconId = BeaconDetector.BeaconTags.LEFT;
+
+    @Override
+    public void initialize() {
+//        PhotonCore.enable();
+        schedule(new BulkCacheCommand(hardwareMap));
+
+        drive = new SampleMecanumDrive(hardwareMap);
+        claw = new Claw(hardwareMap);
+        lift = new Lift(hardwareMap);
+        arm = new Arm(hardwareMap);
+
+        drive.setPoseEstimate(ThreeCycleTrajectories.leftStartingPosition);
+        ThreeCycleTrajectories.clawOpenAction = () -> {
+            claw.open();
+            claw.poleGuideDown();
+        };
+        ThreeCycleTrajectories.clawCloseAction = () -> claw.close();
+        ThreeCycleTrajectories.generateTrajectories(drive);
+        claw.close();
+
+        beaconDetector = new BeaconDetector(hardwareMap, true);
+
+
+        DropPreloadCommand preloadCommand = new DropPreloadCommand(drive, lift, claw, arm, true);
+
+
+        //Start vision
+        beaconDetector.startStream();
+
+        while (!isStarted()) {
+            beaconId = beaconDetector.update();
+            telemetry.addLine("Ready for start!");
+            telemetry.addData("Beacon", beaconId);
+            telemetry.update();
+        }
+
+        beaconDetector.stopStream();
+
+        schedule(new SequentialCommandGroup(
+                preloadCommand,
+                new CRIMiddleLeftParkCommand(drive, lift, arm, claw, beaconId, true, true)
+        ));
+    }
+
+    @Override
+    public void run(){
+        CommandScheduler.getInstance().run();
+
+        telemetry.addData("lift", lift.getLiftPosition());
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autocommands/CRIMiddleLeftParkCommand.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.commands.autocommands;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.commands.TrajectorySequenceCommand;
+import org.firstinspires.ftc.teamcode.commands.presets.RetractOuttakeCommand;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
+import org.firstinspires.ftc.teamcode.vision.BeaconDetector;
+
+public class CRIMiddleLeftParkCommand extends ParallelCommandGroup {
+
+
+    public CRIMiddleLeftParkCommand(SampleMecanumDrive drive, Lift lift, Arm arm, Claw claw,
+                                    BeaconDetector.BeaconTags tag, boolean isLeft, boolean isHigh) {
+
+        addCommands(
+                new SequentialCommandGroup(
+                        new WaitCommand(100),
+                        new InstantCommand(() -> {
+                            lift.setLiftPower(0.8);
+                        }),
+                        new WaitCommand(500),
+                        new InstantCommand(() -> {
+                            lift.setLiftPower(0);
+                        }),
+
+//                        new WaitCommand(800),
+                        new RetractOuttakeCommand(lift, arm, claw)
+                )
+
+        );
+
+        switch (tag) {
+            case CENTER:
+                addCommands(
+                        new TrajectorySequenceCommand(drive,
+                                (isHigh) ?
+                                        (isLeft) ? ThreeCycleTrajectories.leftHighLeftPark : ThreeCycleTrajectories.rightHighRightPark :
+                                        (isLeft) ? ThreeCycleTrajectories.leftLeftPark : ThreeCycleTrajectories.rightRightPark
+                        )
+                );
+                break;
+            case RIGHT:
+                addCommands(
+                        new TrajectorySequenceCommand(drive,
+                                (isHigh) ?
+                                        (isLeft) ? ThreeCycleTrajectories.leftHighMiddlePark : ThreeCycleTrajectories.rightHighMiddlePark :
+                                        (isLeft) ? ThreeCycleTrajectories.leftMiddlePark : ThreeCycleTrajectories.rightMiddlePark
+                        )
+                );
+                break;
+            case LEFT:
+                addCommands(
+                        new TrajectorySequenceCommand(drive,
+                                (isHigh) ?
+                                        (isLeft) ? ThreeCycleTrajectories.leftHighRightPark : ThreeCycleTrajectories.rightHighLeftPark :
+                                        (isLeft) ? ThreeCycleTrajectories.leftRightPark : ThreeCycleTrajectories.rightLeftPark
+                        )
+                );
+                break;
+        }
+
+    }
+}
